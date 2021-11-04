67753b279345ced37d3b146297bbc6cab74c831c
TanyaRS <Ladiesvest@yandex.ru>
2021-11-04 17:12:23 +0500

    Home Work Ver/3 Ыук

--- Banks/Account.java
+++ Banks/Account.java
@@ -1,41 +1,42 @@
 package Banks;
 
-import java.util.Objects;
-
 public class Account{
-    public int scorenumber;
+    public Integer scorenumber;
     public int balance;
+    public Client client;
 
-    public Account(int scorenumber,int balance) {
+    public Account(Integer scorenumber, int balance, Client client) {
         this.scorenumber = scorenumber;
         this.balance = balance;
+        this.client = client;
     }
 
-    @Override
+    /*@Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Account account = (Account) o;
         return scorenumber == account.scorenumber;
-    }
+    }*/
 
-    @Override
+    /*@Override
     public int hashCode() {
         return Objects.hash(scorenumber);
+    }*/
+
+    @Override
+    public boolean equals (Object o){
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Account otherAccount = (Account) o;
+        return this.scorenumber.equals(otherAccount.scorenumber);//Objects.equals(this.scorenumber, otherAccount.scorenumber);
     }
-    //        @Override
-//        public boolean equals (Object o){
-//            if (this == o) return true;
-//            if (o == null) return false;
-//            if (getClass() != o.getClass()) return false;
-//            Account otherAccount = (Account) o;
-//            return this.scorenumber.equals(otherAccount.scorenumber);//Objects.equals(this.scorenumber, otherAccount.scorenumber);
-//        }
-//
-//        @Override
-//        public int hashCode () {
-//            return scorenumber;
-//        }
+
+        @Override
+        public int hashCode () {
+            return scorenumber;
+        }
 
     }
 
--- Banks/Bank.java
+++ Banks/Bank.java
@@ -1,30 +1,79 @@
 package Banks;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Bank {
-
     public static void main(String[] args) {
-        Map<Client, Account> personAccount = getPersonAccount();
+        Map<Client, List<Account>>personAccount = getPersonAccount();
 
-        Client ryzhkovatatyana = new Client ("Рыжкова Татьяна");
-        Account ryzhkovatatyanaAccount = personAccount.get(ryzhkovatatyana);
-        System.out.println(ryzhkovatatyanaAccount.scorenumber);
-    }
+        // нашла клиента тут
+        Client client = findClientByName(personAccount, "Зауербрей Алена");
 
-        private static Map<Client, Account> getPersonAccount() {
-            Client zauerbreyalena = new Client("Зауербрей Алена");
-            Client grishaninsergey = new Client("Гришанин Сергей");
-            Client ryzhkovatatyana = new Client("Рыжкова Татьяна");
+        if (client == null) {
+            System.out.println("No client");
+        } else {
+            // ищем счета по клиенту
+            List<Account> ryzhkovatatyanaAccount = personAccount.get(client);
+            for (Account account : ryzhkovatatyanaAccount) {
+                System.out.println(account.scorenumber + " " + account.balance);
+            }
+        }
 
-            Map<Client, Account> personAccount = new HashMap<>();
-            personAccount.put(zauerbreyalena, new Account(277463787, 2000));
-            personAccount.put(grishaninsergey, new Account(277948499, 1000));
-            personAccount.put(ryzhkovatatyana, new Account(277388847,3000));
-            return personAccount;
+
+        Integer scorenumber = 277463787;
+        Account account = findClientByAccount(personAccount, scorenumber);
+        if (account == null) {
+            System.out.println("no found");
+        } else {
+            System.out.println("scorenumber = " + scorenumber + " Принадлежит " + account.client.name);
         }
     }
 
+    private static Map<Client, List<Account>> getPersonAccount() {
+        Client zauerbreyalena = new Client("Зауербрей Алена");
+        List<Account> zauerbreyalenaAccountList = new ArrayList<>();
+        zauerbreyalenaAccountList.add(new Account(277463787, 2000, zauerbreyalena));
+        zauerbreyalenaAccountList.add(new Account(277463799, 2908685, zauerbreyalena));
 
+       /* Client grishaninsergey = new Client("Гришанин Сергей");
+        Client ryzhkovatatyana = new Client("Рыжкова Татьяна");*/
+
+        Map<Client, List<Account>> personAccount = new HashMap<>();
+        personAccount.put(zauerbreyalena, zauerbreyalenaAccountList);
+      /*  personAccount.put(grishaninsergey, new Account(277948499, 1000));
+        personAccount.put(ryzhkovatatyana, new Account(277388847,3000));*/
+        return personAccount;
+    }
+
+    private static Client findClientByName(Map<Client, List<Account>>personAccount, String clientName) {
+        for (Map.Entry<Client, List<Account>> entry : personAccount.entrySet()) {
+            if (entry.getKey().name.equals(clientName)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    private static Account findClientByAccount(Map<Client, List<Account>> personAccount, Integer scorenumber) {
+        List<Account> accountList = getAllAccounts(personAccount);
+        for (Account account : accountList) {
+            if (account.scorenumber.equals(scorenumber)) {
+                return account;
+            }
+        }
+
+        return null;
+    }
+
+    private static List<Account> getAllAccounts(Map<Client, List<Account>> personAccount) {
+        List<Account> accountList = new ArrayList<>();
+        for (List<Account> clientAccount : personAccount.values()) {
+            accountList.addAll(clientAccount);
+        }
+
+        return accountList;
+    }
+}
